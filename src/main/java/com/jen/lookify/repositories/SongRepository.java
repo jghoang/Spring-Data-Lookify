5b4aa9c410ac468db48b7ce34d9ee677d2eb854c
jghoang <jen.hoang7@icloud.com>
2019-09-01 00:16:06 -0500

    updating settings

---
+++ src/main/java/com/jen/lookify/repositories/SongRepository.java
@@ -0,0 +1,21 @@
+package com.jen.lookify.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jen.lookify.models.Song;
+
+// Class as repo
+@Repository
+public interface SongRepository extends CrudRepository<Song, Long> {
+	// Method overrides
+	@Override
+	List<Song> findAll(); // return list of songs
+
+	List<Song> findByArtistContaining(String search); // find artist based on certain criteria
+
+	List<Song> findTop10ByOrderByRatingDesc(); // query top 10 by rating
+
+}
